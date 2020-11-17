464ae7d69e72557a1505c6220f6232d2bb75fffe
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-11-17 13:30:57 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -10,33 +10,6 @@ class Book
         puts "Flipping the page...wow, you read fast!"
     end
 end
-    # def title
-    #     @title
-    # end
-
-    # def author=(author)
-    #     @author = author
-    # end
-
-    # def author
-    #     @author 
-    # end
-
-    # def page_count=(page_count)
-    #     @page_count = page_count
-    # end
-
-    # def page_count
-    #     @page_count
-    # end
-    
-    # def genre=(genre)
-    #     @genre = genre
-    # end
-
-    # def genre
-    #     @genre
-    # end
 
 
 
